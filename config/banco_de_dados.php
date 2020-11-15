087a6fa61682b3a0c8f124b36b36fd4a509113d1
Antigo <56899690+Antigo-Antigo@users.noreply.github.com>
2020-11-15 12:19:01 -0300

    Update banco_de_dados.php

--- config/banco_de_dados.php
+++ config/banco_de_dados.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'driver' => 'mysql',
-    'servidor' => 'ns208.hostgator.com.br',
-    'banco_de_dados' => 'alangu42_teste_internit',
-    'usuario' => 'alangu42_testnit',
-    'senha' => 'teste_internit'
+    'driver' => '',
+    'servidor' => '',
+    'banco_de_dados' => '',
+    'usuario' => '',
+    'senha' => ''
 ];
